460b88eaa12e6cd79c33c2eb9982a32133b8b94b
ElielM9 <elielm291@gmail.com>
2023-11-08 18:54:05 -0600

    Agregada una validación al formulario

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,40 @@
-console.log(`Hello World with gulp`);
+// Variables
+const form = document.querySelector(`#form`);
+const tasksList = document.querySelector(`#tasksList`);
+let tasks = [];
+
+// Eventos
+document.addEventListener(`DOMContentLoaded`, eventListeners);
+
+function eventListeners() {
+  form.addEventListener(`submit`, addTask);
+}
+
+// Funciones
+function addTask(e) {
+  e.preventDefault();
+
+  //   Input en el que el usuario escribe
+  const formInput = document.querySelector(`#formInput`).value;
+
+  if (formInput === ``) {
+    showError(`No escribiste una tarea`);
+
+    return;
+  }
+}
+
+function showError(error) {
+  const main = document.querySelector(`.main`);
+  const errorMessage = document.createElement(`p`);
+
+  errorMessage.textContent = error;
+  errorMessage.classList.add(`error`);
+
+  main.appendChild(errorMessage);
+
+  //  Borra la alerta pasados 3 segundos
+  setTimeout(() => {
+    errorMessage.remove();
+  }, 3000);
+}
